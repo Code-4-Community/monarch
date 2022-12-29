bab4833cd1a7b5acb1a0a3ad911e53129c7c2f14
Harrison Kim <kim.harr@northeastern.edu>
2022-12-28 21:01:46 -0500

    fix: updated tsconfig.json to include vite.config.ts so that it can import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Link monorepo package properly:
-  // https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies
-  optimizeDeps: {
-    include: ['common'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/common/, /node_modules/],
+    plugins: [react()],
+    // Link monorepo package properly:
+    // https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies
+    optimizeDeps: {
+        include: ['common'],
     },
-  },
-})
+    build: {
+        commonjsOptions: {
+            include: [/common/, /node_modules/],
+        },
+    },
+});
