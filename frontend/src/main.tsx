f80299cff280f24b00db871264c1e9b8167f5c20
Harrison Kim <kim.harr@northeastern.edu>
2022-12-28 20:34:16 -0500

    feature: working implementation of typescript eslint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 const queryClient = new QueryClient()
 
+const t = 3;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
